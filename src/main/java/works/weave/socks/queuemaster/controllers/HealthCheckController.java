24c4d3d42995e22d3c0d72be3ca50d5c1003ed33
Johannes Bräuer <johannes.braeuer@dynatrace.com>
2018-09-05 14:41:16 +0200

    Removed import command.

--- src/main/java/works/weave/socks/queuemaster/controllers/HealthCheckController.java
+++ src/main/java/works/weave/socks/queuemaster/controllers/HealthCheckController.java
@@ -5,7 +5,6 @@ import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.rabbit.core.ChannelCallback;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import works.weave.socks.queuemaster.entities.HealthCheck;
